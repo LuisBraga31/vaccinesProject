c872d5ec0c033e583e8f46eed7f2f823b1860df7
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-01-18 21:16:01 -0300

    Criando mais um teste em Update

--- src/test/java/br/bonnasys/vaccines/domain/usecase/patient/update/UpdatePatientUseCaseImplTest.java
+++ src/test/java/br/bonnasys/vaccines/domain/usecase/patient/update/UpdatePatientUseCaseImplTest.java
@@ -24,7 +24,7 @@ class UpdatePatientUseCaseImplTest {
     private PatientRepository repository;
 
     @Test
-    void check() {
+    void givenValidMapWhenCallsUpdatePatientUseCaseThenAssertFields() {
         String name = "Henrique Gonçalves";
         String newName = "Henrique Tilapia";
 
@@ -37,6 +37,38 @@ class UpdatePatientUseCaseImplTest {
 
         Map<String, Object> fields = new HashMap<>();
         fields.put("name", newName);
+
+        String patientId = createdPatient.getId();
+
+        updatePatientUseCase.execute(patientId, fields);
+
+        Patient retrievedPatient = repository.findById(patientId).orElseThrow();
+        Assertions.assertNotEquals(name, retrievedPatient.getName());
+        Assertions.assertEquals(newName, retrievedPatient.getName());
+
+
+    }
+
+    @Test
+    void givenInvalidMapWhenCallsUpdatePatientUseCaseThenShouldNotThrowsException() {
+        String name = "Pedro José";
+        String phone = "(99) 9999-8888";
+        String email = "pedro.jose@gmail.com";
+        LocalDate birthdate = LocalDate.now().minusYears(32);
+        Patient patient = new Patient(name, phone, email, birthdate);
+
+        Patient createdPatient = repository.save(patient);
+
+        Map<String, Object> fields = new HashMap<>();
+        String newName = "Henrique Tilapia";
+        String newPhone = "(00) 33333-0000";
+        fields.put("name", newName);
+        fields.put("idade", newName);
+        fields.put("telefone", newName);
+        fields.put("email", newName);
+        fields.put("cellphone", newName);
+        fields.put("phone", newPhone);
+
 
         String patientId = createdPatient.getId();
 
