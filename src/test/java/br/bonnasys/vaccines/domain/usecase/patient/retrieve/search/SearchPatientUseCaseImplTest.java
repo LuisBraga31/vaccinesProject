8996d7d0b1e5b6b36626ab0d0cdd193fb0a2c000
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2024-02-06 14:17:02 -0300

    Adicionando testes

--- src/test/java/br/bonnasys/vaccines/domain/usecase/patient/retrieve/search/SearchPatientUseCaseImplTest.java
+++ src/test/java/br/bonnasys/vaccines/domain/usecase/patient/retrieve/search/SearchPatientUseCaseImplTest.java
@@ -1,8 +1,12 @@
 package br.bonnasys.vaccines.domain.usecase.patient.retrieve.search;
 
+import br.bonnasys.vaccines.domain.model.Patient;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,7 +19,21 @@ class SearchPatientUseCaseImplTest {
     private SearchPatientUseCase searchPatientUseCase;
 
     @Test
-    void givenAnValidPageableWhenCallSearchPatientUseCaseThenReturnPatient(){
+    void givenValidPageableWhenCallsSearchPatientUseCaseThenReturnPatients() {
+        Page<Patient> patientPage = searchPatientUseCase.execute(null, PageRequest.of(0, 2));
+        Assertions.assertNotNull(patientPage);
+    }
 
+    @Test
+    void givenACommandWhenCallsSearchPatientUseCaseThenReturnPatients() {
+        Integer expectedContentSize = 2;
+        Long expectedTotalElements = 2L;
+
+        SearchPatientCommand command = new SearchPatientCommand("Ga", null);
+        Page<Patient> patientPage = searchPatientUseCase.execute(command, PageRequest.of(0, 2));
+
+        Assertions.assertNotNull(patientPage);
+        Assertions.assertEquals(expectedContentSize, patientPage.getContent().size());
+        Assertions.assertEquals(expectedTotalElements, patientPage.getTotalElements());
     }
 }
--- src/test/java/br/bonnasys/vaccines/support/builder/PatientBuilder.java
+++ src/test/java/br/bonnasys/vaccines/support/builder/PatientBuilder.java
@@ -4,6 +4,7 @@ import br.bonnasys.vaccines.domain.model.Patient;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 public final class PatientBuilder {
 
@@ -11,21 +12,32 @@ public final class PatientBuilder {
     private String phone;
     private String email;
     private LocalDate birthdate;
+    private OffsetDateTime createdAt;
+    private OffsetDateTime updatedAt;
+    private String id;
 
-    public static PatientBuilder any () {
+    public static PatientBuilder any() {
         PatientBuilder patientBuilder = new PatientBuilder();
         patientBuilder.name = "Jay Pritcher";
         patientBuilder.email = "jay@moveis.com.br";
         patientBuilder.phone = "(99) 99999-9999";
         patientBuilder.birthdate = LocalDate.now().minusYears(21);
-
+        patientBuilder.createdAt = OffsetDateTime.now();
+        patientBuilder.updatedAt = OffsetDateTime.now();
         return patientBuilder;
     }
 
-
-
-    public Patient build() {
-        return new Patient(this.name, this.phone, this.email, this.birthdate);
+    public PatientBuilder name(String name) {
+        this.name = name;
+        return this;
     }
 
+    public PatientBuilder withId() {
+        this.id = UUID.randomUUID().toString();
+        return this;
+    }
+
+    public Patient build() {
+        return new Patient(this.id, this.name, this.phone, this.email, this.birthdate, this.createdAt, this.updatedAt);
+    }
 }
